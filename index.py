459840ffb1f006c575f2f936647641405cbe77c7
juanccm717-cyber <juanccm717@gmail.com>
2025-12-05 14:40:51 -0500

    Arreglo handler

--- index.py
+++ index.py
@@ -58,5 +58,6 @@ def test_conexion():
     except Exception as e:
         return f'<!DOCTYPE html><html><body><h1>Error</h1><pre>{e}</pre></body></html>'
 
+
 def handler(environ, start_response):
     return app(environ, start_response)
